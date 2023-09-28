9bf6c7eccf82d465e33b2dd7f06fd97c927f5935
GADDALA ASHISH <ashishpaul1199@gmail.com>
2023-09-28 14:48:46 +0530

    Add files via upload

---
+++ 02-Write your First C++ Program/Firstprogram.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // 1. Datatypes
+    // int a=10;
+    // char c='a';
+    // float f=1.2;
+    // long l;
+
+    // 1. checking size of data types
+    // 1. int
+    // cout<<sizeof(a)<<endl; //Output: 4
+
+    // 2. char
+    // cout<<sizeof(c)<<endl; //Output: 1
+
+    // 3. float
+    // cout<<sizeof(f)<<endl; //Output: 4
+
+    // 4. long
+    // cout<<sizeof(l)<<endl; //Output:4
+
+    // 2. Printing Characters
+    //   int c=-100;
+    //  int c=-99;
+    //  int c=65;
+    //   cout<<(char)c<<endl;
+    //   return 0;
+
+    // 3. Operators
+    // a. Arithmetic Operator
+    // int a=10;
+    // int b=5;
+    // cout<<a+b<<endl; //Output: 15
+    // cout<<a-b<<endl; //Output: 5
+    // cout<<a*b<<endl; //Output: 50
+    // cout<<a/b<<endl; //Output: 2
+    // cout<<a%b<<endl; //Output: 0
+
+    // Output:
+    // 15
+    // 5
+    // 50
+    // 2
+    // 0
+
+    // b. Relational Operator
+    // int a=20;
+    // int b=10;
+    // cout<<(a>b)<<endl;
+    // cout<<(a<b)<<endl;
+    // cout<<(a<=b)<<endl;
+    // cout<<(a>=b)<<endl;
+    // cout<<(a!=b)<<endl;
+    // cout<<(a==b)<<endl;
+
+    // Output:
+    // 1
+    // 0
+    // 0
+    // 1
+    // 1
+    // 0
+
+    // 4. License Program
+    // int age;
+    // cout << "enter your age:";
+    // cin >> age;
+
+    // int car;
+    // cout << "enter number of cars owned:" << endl;
+    // cin >> car;
+
+    // if (age >= 18 && car >= 1)
+    // {
+    //     cout << "Eligble for license" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not eligible" << endl;
+    // }
+
+    // Output:
+    // enter your age:19
+    // enter number of cars owned:
+    // 2
+    // Eligble for license
+
+    // enter your age:6
+    // enter number of cars owned:
+    // 2
+    // Not eligible
+
+    // 5. Input/Output in C++
+    // int marks;
+    // cout<<"enter marks:"<<endl;
+
+    // cin>>marks;
+    // cout<<"Printing marks:"<<marks<<endl;
+}
