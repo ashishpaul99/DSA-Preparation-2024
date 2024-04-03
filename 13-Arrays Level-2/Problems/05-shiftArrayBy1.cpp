08d85f4f99a57b7cad0c362b8d0b7f877f6568d1
GADDALA ASHISH <ashishpaul1199@gmail.com>
2024-04-03 22:55:15 +0530

    Add files via upload

---
+++ 13-Arrays Level-2/Problems/05-shiftArrayBy1.cpp
@@ -0,0 +1,38 @@
+// 5. shift element of array by 1
+// how to approach?
+// 1. Initialize two pointers, left and right, to the second-to-last and last indices of the array, respectively.
+// 2. Store the value of the last element of the array (arr[right]) in a temporary variable temp.
+// 3. Iterate through the array from right to left (from left to 0):
+// ->Assign the value of the element at arr[left] to the element at arr[right].
+// ->Move the pointers left and right one position to the left.
+// 4. After the loop, assign the value stored in temp to the first element of the array (arr[0]), effectively moving the last element of the array to the beginning.
+// 5. Finally, print the modified array.
+
+// Code:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 6;
+    int arr[n] = {10, 20, 30, 40, 50, 60};
+
+    int left = n - 2, right = n - 1;
+    int temp = arr[right];
+    while (left >= 0)
+    {
+        arr[right] = arr[left];
+
+        left--;
+        right--;
+    }
+    arr[0] = temp;
+
+    // printing array
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+// Output:
+// 60 10 20 30 40 50
