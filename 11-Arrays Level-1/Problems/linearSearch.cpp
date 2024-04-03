335a5176995eed245dc02cc9b48af0f6a6629bbb
GADDALA ASHISH <ashishpaul1199@gmail.com>
2024-04-03 12:46:44 +0530

    Add files via upload

---
+++ 11-Arrays Level-1/Problems/linearSearch.cpp
@@ -0,0 +1,29 @@
+// 1. Linear Search
+// ->using flag
+// ->
+
+#include <iostream>
+using namespace std;
+void linearSearch(int arr[], int key, int n)
+{
+    bool flag = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            flag = 1;
+            cout << "key found at index" << i << endl;
+        }
+    }
+    if (flag == 0)
+    {
+        cout << "key not found" << endl;
+    }
+}
+int main()
+{
+    int size = 5;
+    int arr[size] = {5, 4, 6, 7, 9};
+    int key = 7;
+    linearSearch(arr, key, size);
+}
