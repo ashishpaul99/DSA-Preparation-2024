b4e8a44d9f470776de86da634d43e9735f161bff
GADDALA ASHISH <ashishpaul1199@gmail.com>
2024-04-03 12:52:25 +0530

    Add files via upload

---
+++ 11-Arrays Level-1/Problems/02-countZerosOnes.cpp
@@ -0,0 +1,22 @@
+// 2. counting zeros and ones
+// ->intialize zeroCount and oneCount with zero.
+// ->traverse through an array. 
+// ->if element is equal to zero increment zeroCount by 1 and if not increment oneCount by 1.
+#include<iostream>
+using namespace std;
+int main(){
+    int size=6;
+    int arr[size]={0,1,1,0,1,1};
+    int zeroCount=0,oneCount=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]==0){
+            zeroCount++;
+        }else{
+            oneCount++;
+        }
+    }
+    cout<<zeroCount<<" "<<oneCount++<<endl;
+}
+
+// Output:
+// 2 4
