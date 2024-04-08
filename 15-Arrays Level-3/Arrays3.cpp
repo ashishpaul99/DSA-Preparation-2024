fd75988f93fc7c6e0f4703bdab34eadadf75c718
GADDALA ASHISH <ashishpaul1199@gmail.com>
2024-04-08 19:27:06 +0530

    Update Arrays3.cpp

--- 15-Arrays Level-3/Arrays3.cpp
+++ 15-Arrays Level-3/Arrays3.cpp
@@ -1,25 +1,25 @@
 // 15-Arrays Level-3       Date:07/04/24  Time:4:16 PM
 // 1. 2D-Array
 // 1.1 Declaring 2-D Array
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     // Declaring 2-D Array
-//     // int arr[3][4];
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Declaring 2-D Array
+    // int arr[3][4];
 
-//     // Intialise 2-D Array
-//     int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
-// }
+    // Intialise 2-D Array
+    int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
+}
 
 // 1.2 Intializing 2-D Array
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     // Intialise 2-D Array
-//     int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Intialise 2-D Array
+    int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
+}
 
 // 1.3  Intilaizing 2-D Array with column size only
 // ->In order to intialize 2-D Array atleast size of column should be given.
@@ -31,23 +31,23 @@
 // 1.3.1 Intializing 2-D Arrays without mentioning row size
 // ->it works
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
+#include <iostream>
+using namespace std;
+int main()
+{
 
-//     int arr[][4] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
-// }
+    int arr[][4] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
+}
 
 // 1.3.2 Intializing 2-D Arrays without mentioning row and column size
 // ->it gives error
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
+#include <iostream>
+using namespace std;
+int main()
+{
 
-//     int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
-// }
+    int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
+}
 
 // Output:
 // error: declaration of 'arr' as multidimensional array must have bounds for
@@ -55,33 +55,33 @@
 
 // 1.3.3 Intializing 2-D Arrays without mentioning row size
 // ->it gives error
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
+#include <iostream>
+using namespace std;
+int main()
+{
 
-//     int arr[3][] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
-// }
+    int arr[3][] = {{1, 2, 3, 4}, {5, 6, 7, 4}, {20, 43, 2, 26}};
+}
 
 // 2. Printing 2-D Array
-// #include<iostream>
-// using namespace std;
-// void printArray(int arr[2][3],int row,int column){
-//    for(int i=0;i<row;i++){
-//     for(int j=0;j<column;j++){
-//            cout<<arr[i][j]<<" ";
-//     }
-//     cout<<endl;
-//    }
-// }
-// int main(){
+#include<iostream>
+using namespace std;
+void printArray(int arr[2][3],int row,int column){
+   for(int i=0;i<row;i++){
+    for(int j=0;j<column;j++){
+           cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+   }
+}
+int main(){
 
-//     int arr[2][3]={{1,2,3},{4,5,6}};
-//     int row=2;
-//     int column=3;
-//     printArray(arr,row,column);
+    int arr[2][3]={{1,2,3},{4,5,6}};
+    int row=2;
+    int column=3;
+    printArray(arr,row,column);
 
-// }
+}
 
 // Output:
 // 1 2 3
@@ -93,24 +93,24 @@
 // 4 5 6              2 5
 //                    3 6
 
-// #include<iostream>
-// using namespace std;
-// void colWisePrint(int arr[2][3],int row,int column){
-//    for(int i=0;i<column;i++){
-//     for(int j=0;j<row;j++){
-//            cout<<arr[j][i]<<" ";
-//     }
-//     cout<<endl;
-//    }
-// }
-// int main(){
+#include<iostream>
+using namespace std;
+void colWisePrint(int arr[2][3],int row,int column){
+   for(int i=0;i<column;i++){
+    for(int j=0;j<row;j++){
+           cout<<arr[j][i]<<" ";
+    }
+    cout<<endl;
+   }
+}
+int main(){
 
-//     int arr[2][3]={{1,2,3},{4,5,6}};
-//     int row=2;
-//     int column=3;
-//     colWisePrint(arr,row,column);
+    int arr[2][3]={{1,2,3},{4,5,6}};
+    int row=2;
+    int column=3;
+    colWisePrint(arr,row,column);
 
-// }
+}
 
 // Output:
 // 1 4
@@ -118,27 +118,27 @@
 // 3 6
 
 // 3. How to take input for 2-D Array
-// #include<iostream>
-// using namespace std;
-// int main(){
-//    int row=3,col=3;
-//    int arr[3][3];
+#include<iostream>
+using namespace std;
+int main(){
+   int row=3,col=3;
+   int arr[3][3];
 
-//    for(int i=0;i<row;i++){
-//       for(int j=0;j<col;j++){
-//             cout<<"enter element at indexes -> "<<"("<<i<<","<<j<<")"<<":";
-//             cin>>arr[i][j];
-//       }
-//    }
+   for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+            cout<<"enter element at indexes -> "<<"("<<i<<","<<j<<")"<<":";
+            cin>>arr[i][j];
+      }
+   }
 
-//    //Printing 2-D Array
-//    for(int i=0;i<row;i++){
-//       for(int j=0;j<col;j++){
-//             cout<<arr[i][j]<<" ";
-//       }
-//       cout<<endl;
-//    }
-// }
+   //Printing 2-D Array
+   for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+      }
+      cout<<endl;
+   }
+}
 
 // output:
 // enter element at indexes -> (0,0):1
@@ -165,99 +165,99 @@
 // ->if 70 is present print true.
 // ->if 70 not found print false.
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// bool linearSearch(int arr[][3],int row, int col,int target){
-//    for(int i=0;i<row;i++){
-//       for(int j=0;j<col;j++){
-//           if(arr[i][j]==target){
-//             // if target found, return true
-//               return true;
-//           }
-//       }
-//    }
-//    // if target not found after checking all elements
-//    return false;
-// }
-// int main(){
-//    int row=3;
-//    int col=3;
-//    int target=70;
-//    int arr[3][3]={{10,20,30},{40,50,60},{70,80,90}};
-//    bool result=linearSearch(arr,row,col,target);
-//    // cout<<result<<endl;
-//    if(result){
-//        cout<<"element found"<<endl;
-//    }
-//    else{
-//       cout<<"element not found"<<endl;
-//    }
-// }
+bool linearSearch(int arr[][3],int row, int col,int target){
+   for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+          if(arr[i][j]==target){
+            // if target found, return true
+              return true;
+          }
+      }
+   }
+   // if target not found after checking all elements
+   return false;
+}
+int main(){
+   int row=3;
+   int col=3;
+   int target=70;
+   int arr[3][3]={{10,20,30},{40,50,60},{70,80,90}};
+   bool result=linearSearch(arr,row,col,target);
+   // cout<<result<<endl;
+   if(result){
+       cout<<"element found"<<endl;
+   }
+   else{
+      cout<<"element not found"<<endl;
+   }
+}
 
 // Output:
 // element found
 
 // 5. Find maximum number in an array
-// #include<iostream>
-// #include<limits.h>
-// using namespace std;
+#include<iostream>
+#include<limits.h>
+using namespace std;
 
-// void findMaximum(int arr[][3],int row, int col){
-//    int max=INT_MIN;
-//    for(int i=0;i<row;i++){
-//       for(int j=0;j<col;j++){
-//          if(arr[i][j]>max){
-//             max=arr[i][j];
-//          }
-//       }
-//    }
-//    cout<<"Printing maximum number in array: "<<max<<endl;
-// }
+void findMaximum(int arr[][3],int row, int col){
+   int max=INT_MIN;
+   for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+         if(arr[i][j]>max){
+            max=arr[i][j];
+         }
+      }
+   }
+   cout<<"Printing maximum number in array: "<<max<<endl;
+}
 
-// void findMinimum(int arr[][3],int row, int col){
-//    int min=INT_MAX;
-//    for(int i=0;i<row;i++){
-//       for(int j=0;j<col;j++){
-//           if(arr[i][j]<min){
-//             min=arr[i][j];
-//           }
-//       }
-//    }
-//    cout<<"Printing minmum number in array: "<<min<<endl;
-// }
+void findMinimum(int arr[][3],int row, int col){
+   int min=INT_MAX;
+   for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+          if(arr[i][j]<min){
+            min=arr[i][j];
+          }
+      }
+   }
+   cout<<"Printing minmum number in array: "<<min<<endl;
+}
 
-// int main(){
-//    int row=3;
-//    int col=3;
-//    int arr[3][3]={{10,90,30},{40,70,60},{50,80,20}};
-//    findMaximum(arr,row,col);
-//    findMinimum(arr,row,col);
-// }
+int main(){
+   int row=3;
+   int col=3;
+   int arr[3][3]={{10,90,30},{40,70,60},{50,80,20}};
+   findMaximum(arr,row,col);
+   findMinimum(arr,row,col);
+}
 
 // Output:
 // Printing maximum number in array: 90
 // Printing minmum number in array: 10
 
 // 6. row wise sum
-// #include<iostream>
-// using namespace std;
-// void rowSum(int arr[3][4],int row,int col){
-//    for(int i=0;i<row;i++){
-//       int sum=0;
-//       for(int j=0;j<col;j++){
-//          sum=sum+arr[i][j];
-//       }
-//       cout<<"sum of row "<<i<<":"<<sum<<endl;
-//    }
-// }
-// int main(){
-//    int arr[3][4]={{1,2,3,4},{5,6,7,8},{9,10,11}};
-//    int row=3;
-//    int col=4;
-//    rowSum(arr,row,col);
+#include<iostream>
+using namespace std;
+void rowSum(int arr[3][4],int row,int col){
+   for(int i=0;i<row;i++){
+      int sum=0;
+      for(int j=0;j<col;j++){
+         sum=sum+arr[i][j];
+      }
+      cout<<"sum of row "<<i<<":"<<sum<<endl;
+   }
+}
+int main(){
+   int arr[3][4]={{1,2,3,4},{5,6,7,8},{9,10,11}};
+   int row=3;
+   int col=4;
+   rowSum(arr,row,col);
 
-// }
+}
 
 // Output:
 // sum of row 0:10
@@ -266,27 +266,27 @@
 
 // 7. column wise sum
 
-// #include <iostream>
-// using namespace std;
-// void rowSum(int arr[3][4], int row, int col)
-// {
-//    for (int i = 0; i < col; i++)
-//    {
-//       int sum = 0;
-//       for (int j = 0; j < row; j++)
-//       {
-//          sum = sum + arr[j][i];
-//       }
-//       cout <<"sum of column "<<i<<":"<<sum<<endl;
-//    }
-// }
-// int main()
-// {
-//    int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-//    int row = 3;
-//    int col = 4;
-//    rowSum(arr, row, col);
-// }
+#include <iostream>
+using namespace std;
+void rowSum(int arr[3][4], int row, int col)
+{
+   for (int i = 0; i < col; i++)
+   {
+      int sum = 0;
+      for (int j = 0; j < row; j++)
+      {
+         sum = sum + arr[j][i];
+      }
+      cout <<"sum of column "<<i<<":"<<sum<<endl;
+   }
+}
+int main()
+{
+   int arr[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+   int row = 3;
+   int col = 4;
+   rowSum(arr, row, col);
+}
 
 // Output:
 // sum of column 0:15
@@ -302,73 +302,73 @@
 
 // ->Diagonal sum = 1+6+11+16 = 34
 
-// #include <iostream>
-// using namespace std;
-// void diagonalSum(int arr[4][4], int row, int col)
-// {
-//    int sum = 0;
-//    for (int i = 0; i < col; i++)
-//    {
+#include <iostream>
+using namespace std;
+void diagonalSum(int arr[4][4], int row, int col)
+{
+   int sum = 0;
+   for (int i = 0; i < col; i++)
+   {
 
-//       for (int j = 0; j < row; j++)
-//       {
-//          if(i==j){
-//              sum = sum + arr[j][i];
-//          }
-//       }
-//    }
-//    cout <<"sum of diagonal elements:"<<sum<<endl;
-// }
-// int main()
-// {
-//    int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12},{13,14,15,16}};
-//    int row = 4;
-//    int col = 4;
-//    diagonalSum(arr, row, col);
-// }
+      for (int j = 0; j < row; j++)
+      {
+         if(i==j){
+             sum = sum + arr[j][i];
+         }
+      }
+   }
+   cout <<"sum of diagonal elements:"<<sum<<endl;
+}
+int main()
+{
+   int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12},{13,14,15,16}};
+   int row = 4;
+   int col = 4;
+   diagonalSum(arr, row, col);
+}
 
 // Output:sum of diagonal elements:34
 
 // Alternative
-// #include <iostream>
-// using namespace std;
-// void diagonalSum(int arr[4][4], int row, int col)
-// {
-//    int sum = 0;
-//    for (int i = 0; i < col; i++)
-//    {
-//       sum = sum + arr[i][i];
-//    }
-//    cout << "diagonal sum: " << sum << endl;
-// }
-// int main()
-// {
-//    int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-//    int row = 4;
-//    int col = 4;
-//    diagonalSum(arr, row, col);
-// }
+#include <iostream>
+using namespace std;
+void diagonalSum(int arr[4][4], int row, int col)
+{
+   int sum = 0;
+   for (int i = 0; i < col; i++)
+   {
+      sum = sum + arr[i][i];
+   }
+   cout << "diagonal sum: " << sum << endl;
+}
+int main()
+{
+   int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+   int row = 4;
+   int col = 4;
+   diagonalSum(arr, row, col);
+}
 
 // Output:
 // diagonal sum: 34
 
 // 9. Diagonal Print
-// #include <iostream>
-// using namespace std;
-// void diagonalSum(int arr[4][4], int row, int col)
-// {
-//    for (int i = 0; i < col; i++)
-//    {
-//       cout<<arr[i][i]<<" ";
-//    }
-// }
-// int main()
-// {
-//    int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-//    int row = 4;
-//    int col = 4;
-//    diagonalSum(arr, row, col);
-// }
+#include <iostream>
+using namespace std;
+void diagonalSum(int arr[4][4], int row, int col)
+{
+   for (int i = 0; i < col; i++)
+   {
+      cout<<arr[i][i]<<" ";
+   }
+}
+int main()
+{
+   int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+   int row = 4;
+   int col = 4;
+   diagonalSum(arr, row, col);
+}
 
 // Output:
 // 1 6 11 16
@@ -381,21 +381,21 @@
 
 // Diagonal elements= 13,10,7,4
 
-// #include <iostream>
-// using namespace std;
-// void diagonalSum(int arr[4][4], int row, int col)
-// {
-//      for(int i=row-1,j=0;i>=0;i--,j++){
-//         cout<<arr[i][j]<<" ";
-//      }
-// }
-// int main()
-// {
-//    int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-//    int row = 4;
-//    int col = 4;
-//    diagonalSum(arr, row, col);
-// }
+#include <iostream>
+using namespace std;
+void diagonalSum(int arr[4][4], int row, int col)
+{
+     for(int i=row-1,j=0;i>=0;i--,j++){
+        cout<<arr[i][j]<<" ";
+     }
+}
+int main()
+{
+   int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+   int row = 4;
+   int col = 4;
+   diagonalSum(arr, row, col);
+}
 
 // Output: 13 10 7 4
 
@@ -403,34 +403,34 @@
 // 1 2 3                  1 4 8
 // 4 6 7        ----->    2 6 7
 // 8 9 10                 3 7 10
-// #include <iostream>
-// using namespace std;
-// void transposeMatrix(int arr[3][3], int row, int col)
-// {
-//     for (int i = 0; i < row; i++)
-//     {
-//         for (int j = i; j < col; j++)
-//         {
-//             swap(arr[i][j], arr[j][i]);
-//         }
-//     }
+#include <iostream>
+using namespace std;
+void transposeMatrix(int arr[3][3], int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = i; j < col; j++)
+        {
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
 
-//     for (int i = 0; i < row; i++)
-//     {
-//         for (int j = 0; j < col; j++)
-//         {
-//             cout << arr[i][j] << " ";
-//         }
-//         cout << endl;
-//     }
-// }
-// int main()
-// {
-//     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-//     int row = 3;
-//     int col = 3;
-//     transposeMatrix(arr, row, col);
-// }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int row = 3;
+    int col = 3;
+    transposeMatrix(arr, row, col);
+}
 
 // Output:
 // 1 4 7
@@ -438,76 +438,76 @@
 // 3 6 9
 
 // 11. 1-D Vector
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//     int n=5;
-//     vector<int> v(n);
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n=5;
+    vector<int> v(n);
 
-//     for(int i=0;i<v.size();i++){
-//         cout<<v[i]<<" ";
-//     }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
 
-// }
+}
 
 // Output:
 // 0 0 0 0 0
 
 // eg-1:
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//     int n=5;
-//     vector<int> v(n,-1);
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n=5;
+    vector<int> v(n,-1);
 
-//     for(int i=0;i<v.size();i++){
-//         cout<<v[i]<<" ";
-//     }
-// }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+}
 // Output:
 // -1 -1 -1 -1 -1
 
 // 11.1 Inserting elements in vector
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//     vector<int> v;
-//     v.push_back(2);
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(2);
 
-//     for(int i=0;i<v.size();i++){
-//         cout<<v[i]<<" ";
-//     }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
 
-//     // checking size of vector
-//     cout<<v.size()<<endl;
+    // checking size of vector
+    cout<<v.size()<<endl;
 
-//     // checking wheather vector is empty or not
-//     // it returns true or false
-//     cout<<v.empty()<<endl;
-// }
+    // checking wheather vector is empty or not
+    // it returns true or false
+    cout<<v.empty()<<endl;
+}
 
 // Output:
 // 2
 
 // 12- 2-D Vector
 
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//     vector<vector<int>>arr(5,vector<int>(10,5));
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<vector<int>>arr(5,vector<int>(10,5));
 
-//     // row size in 2-d vector -> size of vector
-//     for(int i=0;i<arr.size();i++){
-//         for(int j=0;j<arr[i].size();j++){
-//             cout<<arr[i][j]<<" ";
-//         }
-//         cout<<endl;
-//     }
-// }
+    // row size in 2-d vector -> size of vector
+    for(int i=0;i<arr.size();i++){
+        for(int j=0;j<arr[i].size();j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 // Output:
 // 5 5 5 5 5 5 5 5 5 5
