03650dcb47c2ecc11ac5d605dbc55dff24abdc68
GADDALA ASHISH <ashishpaul1199@gmail.com>
2024-05-09 19:37:31 +0530

    Update 05-shiftArrayBy1.cpp

--- Problems/01-Arrays/05-shiftArrayBy1.cpp
+++ Problems/01-Arrays/05-shiftArrayBy1.cpp
@@ -1,5 +1,18 @@
 // 5. shift element of array by 1
 https://www.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+Solution-1: 
+void rotate(int arr[], int n)
+{  
+    int temp=arr[n-1];
+    for(int i=n-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;   
+}
+
+
+Solution-2
 // how to approach?
 // 1. Initialize two pointers, left and right, to the second-to-last and last indices of the array, respectively.
 // 2. Store the value of the last element of the array (arr[right]) in a temporary variable temp.
